95f4b84b21c6ee2b7ba9c5c073acb1fb08fad5f9
annassetiawan <setiawanannas123@gmail.com>
2020-09-07 18:00:53 +0700

    add details Page

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,39 @@
-import { createDetail } from '../templates/template-creator';
+import { createDetail, createMenuFoodTemplate, createMenuDrinkTemplate } from '../templates/template-creator';
+import RestaurantSource from '../../data/restaurant-source';
+import UrlParser from '../../routes/url-parser';
+import buttonInitiator from '../../utils/button-initiator';
 
 const Detail = {
   async render() {
-   
+    const body = document.querySelector('body');
+    while (body.firstChild) {
+      body.firstChild.remove();
+    }
+    const div = document.createElement('div');
+    div.classList.add('body');
+    body.appendChild(div);
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await RestaurantSource.detailRestaurant(url.id);
+    div.innerHTML += createDetail(restaurant.restaurant);
   },
 
   async afterRender() {
-    
+    buttonInitiator();
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurants = await RestaurantSource.detailRestaurant(url.id);
+    const menuDetailFood = document.querySelector('.menu_details_foods');
+    const menuDetailDrink = document.querySelector('.menu_details_drinks');
+    const { foods } = restaurants.restaurant.menus;
+    const { drinks } = restaurants.restaurant.menus;
+    // const listFoods = foods.map((e) => e.name);
+    // console.log(listFoods);
+    console.log(foods);
+    foods.forEach((food) => {
+      menuDetailFood.innerHTML += createMenuFoodTemplate(food);
+    });
+    drinks.forEach((drink) => {
+      menuDetailDrink.innerHTML += createMenuDrinkTemplate(drink);
+    });
   },
 
 };
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -2,14 +2,18 @@
 import CONFIG from '../../globals/config';
 import locationIcon from '../../../assets/location.svg';
 import starIcon from '../../../assets/star.svg';
+import arrowIcon from '../../../assets/leftarrow.svg';
+import foodIcon from '../../../assets/food.svg';
+import drinkIcon from '../../../assets/drink.svg';
+import menuFoodIcon from '../../../assets/menufood.svg';
+import menuDrinkIcon from '../../../assets/menudrink.svg';
 
 const createButtonTemplate = (city) => ` 
 <button id="button" class="button" data-name=${city}>${city}</button>`;
 
 const createMenuItemTemplate = (restaurant) => `<div class="content hide active" data-name=${restaurant.city}>
     <div class="content__inner ">
-      <div class="content__img" style="background: url(${CONFIG.BASE_IMAGE_URL}small/${restaurant.pictureId});background-position: center;">
-        
+      <div class="content__img" style="background: url(${CONFIG.BASE_IMAGE_URL}small/${restaurant.pictureId});background-position: center;"> 
       </div>
       <div class="content__details">
         <div class="content__place">
@@ -32,8 +36,75 @@ const createMenuItemTemplate = (restaurant) => `<div class="content hide active"
     </div>
   </div>`;
 
-const createDetail = () => '<h2>DETAIL</h2>';
+const createMenuFoodTemplate = (food) => `<div class="menu_details_foods_item">
+  <img src=${menuFoodIcon} alt="menu-icon">
+  <p>${food.name}</p>
+</div>
+<hr class="hrdetails">
+`;
+
+const createMenuDrinkTemplate = (drink) => `<div class="menu_details_foods_item">
+  <img src=${menuDrinkIcon} alt="menu-icon">
+  <p>${drink.name}</p>
+</div>
+<hr class="hrdetails">
+`;
+
+const createDetail = (restaurant) => `
+<a href="#mainContent" class="skip-link">Menuju ke konten</a>
+      <header class="header header__details">
+        <div class="header__inner container">
+          <div class="details__nav">
+            <button class="button_back"><img src=${arrowIcon} alt="arrow-icon"></button>
+          </div>  
+        </div>
+      </header>
+
+      <div class="details__pic" style="background: url(${CONFIG.BASE_IMAGE_URL}medium/${restaurant.pictureId});background-position: center;">
+        <div class="details__overlay">
+            <div class="hero__inner">
+              <h1 class="">${restaurant.name}</h1>
+              <p>${restaurant.address}</p>
+              <p>Categories : ${restaurant.categories.map((category) => category.name)}</p>
+              <div class="content__rating content__rating__details">
+                <img src=${starIcon} alt="star-icon">
+                <span>${restaurant.rating}</span>
+              </div>
+            </div>
+        </div>
+      </div>
+      <main id="mainContent">
+        <div class="main_details container">
+          <div class="button_list_inner button_details_content ">
+            <button id="button" class="button all active_btn" >Menu</button>
+            <button id="button" class="button all " >About</button>
+            <button id="button" class="button all " >Reviews</button>
+          </div>
+          <div clas="menu_details container">
+            <div class="menu_details_categories container">
+              <img src=${foodIcon} alt="food-icon">
+              <h2>Food</h2>
+            </div>
+          <div class="menu_details_foods container"></div>
+          </div>
+          <div clas="menu_details container">
+            <div class="menu_details_categories container">
+              <img src=${drinkIcon} alt="drink-icon">
+              <h2>Drink</h2>
+            </div>
+          <div class="menu_details_drinks container"></div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>Copyright © 2020 - Hunger App</p>
+      </footer>
+    `;
 
 export {
-  createMenuItemTemplate, createDetail, createButtonTemplate,
+  createMenuItemTemplate,
+  createDetail,
+  createButtonTemplate,
+  createMenuFoodTemplate,
+  createMenuDrinkTemplate,
 };
